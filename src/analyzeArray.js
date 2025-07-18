946d278bb8f145125cd24d36238fc4efc3a0ada3
Aaron Henry <aaronhenry1994@outlook.com>
2025-07-18 08:14:16 -0600

    Found average, min, max, and length of the array.

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,11 +1,19 @@
 function analyseArray(arr) {
-    
-        newArr = arr.split();
-        console.log(arr)
-        return newArr;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    const average = sum / arr.length;
 
+    let sorted = arr.toSorted();
+    const min = sorted[0];
+    const max = sorted[sorted.length - 1];
+    const length = arr.length;
+    
+    return { average, min, max, length};
 };
 
-analyseArray([1,8,3,4,2,6])
+array = [1,8,3,4,2,6]
+analyseArray([1,8,3,4,2,6]);
 
 module.exports = analyseArray;
